de9402ff15e9b42bde8b4357e751da99a225eff5
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-03-22 16:01:30 +0900

    highlight

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const words = ["寿司", "刺身", "鮨", "鯖", "軍艦"]; // タイピング練習用の言葉
 const romajiWords = ["sushi", "sashimi", "sushi", "saba", "gunkan"]; // ローマ字表記
 const currentWord = document.getElementById("current-word");
+const romajiWord = document.getElementById("romaji-word");
 const inputField = document.getElementById("input-field");
 const timer = document.getElementById("timer");
 const progress = document.getElementById("progress");
@@ -21,7 +22,21 @@ function showNextWord() {
   activeWord = nextWord.word;
   activeRomaji = nextWord.romaji;
   currentWord.innerText = activeWord;
+  romajiWord.innerHTML = activeRomaji.split('').map(char => `<span>${char}</span>`).join('');
   inputField.value = "";
+}
+
+function updateHighlight() {
+  const input = inputField.value;
+  const romajiChars = romajiWord.getElementsByTagName("span");
+
+  for (let i = 0; i < romajiChars.length; i++) {
+    if (input[i] === activeRomaji[i]) {
+      romajiChars[i].classList.add("highlight");
+    } else {
+      romajiChars[i].classList.remove("highlight");
+    }
+  }
 }
 
 function updateStats() {
@@ -42,6 +57,8 @@ function startTimer() {
 }
 
 inputField.addEventListener("input", () => {
+  updateHighlight();
+
   if (inputField.value === activeRomaji) {
     correctWords++;
     showNextWord();
